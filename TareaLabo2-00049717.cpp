fd9a3a775738f940dec45024790b54ba9f45fd91
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2019-08-29 23:05:24 -0600

    trabajanding

--- TareaLabo2-00049717.cpp
+++ TareaLabo2-00049717.cpp
@@ -6,94 +6,6 @@
 
 using namespace std;
 
-struct pilas
-{
-  int d;
-  pilas *a;
-} *c, *e;
-
-void menu (void);
-void ingresar (void);
-void actualizar_pila (void);
-
-main ()
-{
-  menu ();
-}
-
-
-void
-menu (void)
-{
-  int y, opc;
-  for (;;)
-    {
-      cout << "\n1. Ingresar datos";
-      cout << "\t0. Terminar";
-      cout << "\n Ingrese opcion: ";
-      cin >> opc;
-      switch (opc)
-	{
-	case 1:
-	  ingresar ();
-	  break;
-	case 0:
-	  exit (1);
-	default:
-	  cout << "\n Opcion no valida!!";
-	  break;
-	}
-      actualizar_pila ();
-      cout << "\n\nOprima una tecla para continuar";
-      getch ();
-    }
-}
-
-void
-ingresar (void)
-{
-  if (!c)
-    {
-      c = new (pilas);
-      cout << "Ingrese elemento: ";
-      cin >> c->d;
-      c->a = NULL;
-      return;
-    }
-
-  e = new (pilas);
-  cout << "\nIngrese elemento: ";
-  cin >> e->d;
-  e->a = c;
-  c = e;
-}
-
-void
-actualizar_pila (void)
-{
-  int y = 2, i, ca = 0;
-  e = c;
-  while (e)
-    {
-      ca++;
-      e = e->a;
-    }
-
-  for (i = 0; i <= ca; i++)
-    {
-      cout << " ";
-    }
-  //muestro lo que tiene la pila!!
-  i = 0;
-  e = c;
-  while (e)
-    {
-      cout << "\n";
-      cout << ++i << " - " << e->d;
-      e = e->a;
-    }
-}
-
 struct Nodo{
   int dato;
   Nodo *sig;
@@ -140,16 +52,31 @@ int main()
 {
     Pila pila1;
 
-    int numeros[5];
+    int numeros[5], *num, par, impar;
     cout << "Por favor ingrese 5 nùmeros: \n";
     for(int i = 0; i < 5; i++)
     {
         cin >> numeros[i];
     }
 
-    pila1.push(numeros[5]); 
+    num = numeros;
 
-    pila1.mostrarPila; 
+   for(int i = 0; i < 5; i++)
+    {
+        if (*num%2==0){
+          cout<<"numero par "<<*num<<endl;  
+          *num = par; 
+        }
+        else if(*num%2!=0){
+          cout<<"numero impar "<<*num<<endl;
+          *num = impar; 
+        }
+        num ++; 
+    }
 
+    cout << par << endl;
+    cout << impar << endl; 
+
+    return 0; 
 
 }
