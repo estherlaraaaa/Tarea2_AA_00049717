fbf80d20fe8ca206714132fe1f5f99fcc7007b58
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2019-08-30 10:16:45 -0600

    ya ve que pila tiene mas numeros

--- TareaLabo2-00049717.cpp
+++ TareaLabo2-00049717.cpp
@@ -48,12 +48,49 @@ class Pila{
     }
 };
 
+class Pila2{
+  public:
+    Nodo *crearNodo(int valor){
+      Nodo *n = new Nodo;
+      n->dato = valor;
+      n->sig=nullptr;
+      return n;
+    }
+
+    void push(int valor){
+      Nodo *n = crearNodo(valor);
+      n->sig = inicio;
+      inicio = n;
+    }
+
+    void pop(){
+      Nodo *temp = inicio;
+      inicio = inicio -> sig; 
+      free(temp);
+    }
+
+    void mostrarPila(){
+      Nodo *temp = inicio;
+      if(!inicio)
+        cout<<"the stack is empty"<<endl;
+      else
+      while(temp){
+        cout<< temp -> dato << ",";
+        temp = temp ->sig;
+      }
+    }
+
+    Pila2(){
+      inicio = nullptr;
+    }
+};
+
 int main()
 {
     Pila pila1;
-    Pila pila2; 
+    Pila2 pila2; 
 
-    int numeros[5], *num, par, impar;
+    int numeros[5], *num, par=0, impar=0;
     cout << "Por favor ingrese 5 nùmeros: \n";
     for(int i = 0; i < 5; i++)
     {
@@ -65,27 +102,30 @@ int main()
    for(int i = 0; i < 5; i++)
     {
         if (*num%2==0){
-          cout<<"numero par "<<*num<<endl;  
-          *num = par; 
+          *num = par++;
         }
         else if(*num%2!=0){
-          cout<<"numero impar "<<*num<<endl;
-          *num = impar; 
+          *num = impar++; 
         }
         num ++; 
     }
 
-    cout << par << endl;
-    cout << impar << endl; 
-
+    
     pila1.push(par);
     pila2.push(impar);
-
+    
     cout << "pila1" << endl;
     pila1.mostrarPila();
+    cout << "" << endl;
 
     cout << "pila2" << endl;
     pila2.mostrarPila(); 
+    cout << "" << endl;
+    
+    if (par > impar){
+        cout << "la pila 1 tiene mas numeros "<< endl;
+    }else
+        cout << "la pila 2 tiene mas numeros " << endl;
 
     return 0; 
 
