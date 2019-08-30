8327f5910736184cc8cfe0fe11609b783ab1b0f8
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2019-08-29 22:02:54 -0600

    implementando codigo de labo

--- TareaLabo2-00049717.cpp
+++ TareaLabo2-00049717.cpp
@@ -93,3 +93,54 @@ actualizar_pila (void)
       e = e->a;
     }
 }
+
+struct Nodo{
+  int dato;
+  Nodo *sig;
+}*inicio;
+
+class Pila{
+  public:
+    Nodo *crearNodo(int valor){
+      Nodo *n = new Nodo;
+      n->dato = valor;
+      n->sig=nullptr;
+      return n;
+    }
+
+    void push(int valor){
+      Nodo *n = crearNodo(valor);
+      n->sig = inicio;
+      inicio = n;
+    }
+
+    void pop(){
+      Nodo *temp = inicio;
+      inicio = inicio -> sig; 
+      free(temp);
+    }
+
+    void mostrarPila(){
+      Nodo *temp = inicio;
+      if(!inicio)
+        cout<<"the stack is empty"<<endl;
+      else
+      while(temp){
+        cout<< temp -> dato << ",";
+        temp = temp ->sig;
+      }
+    }
+
+    Pila(){
+      inicio = nullptr;
+    }
+};
+
+int main()
+{
+Pila pila1;
+
+ pila1.push(10);
+ 
+ pila1.mostrarPila();
+}
